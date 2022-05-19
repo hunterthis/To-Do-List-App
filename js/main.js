7fa8301efd4ba5f79b717937dae71339cf851ae7
Hunter <46310167+hunterthis@users.noreply.github.com>
2022-05-19 15:01:50 -0700

    got text into boxes issue fixed, still missing bit of validation procedure but its ok for now

--- js/main.js
+++ js/main.js
@@ -31,6 +31,8 @@ function displayToDoItem(item) {
     var itemText = document.createElement("p");
     itemText.innerHTML = item.title;
     var itemDiv = document.createElement("div");
+    itemDiv.setAttribute("data-task-title", item.title);
+    itemDiv.onclick = toggleComplete;
     itemDiv.classList.add("todo");
     if (item.isCompleted) {
         itemDiv.classList.add("completed");
@@ -39,10 +41,7 @@ function displayToDoItem(item) {
     if (item.isCompleted) {
         itemDiv.classList.add("completed");
     }
-    function markAsComplete() {
-        var itemDiv = this;
-        itemDiv.classList.add("completed");
-    }
+    itemDiv.appendChild(itemText);
     if (item.isCompleted) {
         var completedToDos = document.getElementById("complete-items");
         completedToDos.appendChild(itemDiv);
@@ -50,5 +49,27 @@ function displayToDoItem(item) {
     else {
         var incompleteToDos = document.getElementById("incomplete-items");
         incompleteToDos.appendChild(itemDiv);
+    }
+    function markAsComplete() {
+        var itemDiv = this;
+        itemDiv.classList.add("completed");
+        console.log("Item div is:");
+        console.log(itemDiv);
+    }
+    function toggleComplete() {
+        var itemDiv = this;
+        console.log("Item div is:");
+        console.log(itemDiv);
+        if (itemDiv.classList.contains("completed")) {
+            itemDiv.classList.remove("completed");
+            var incompleteItems = document.getElementById("incomplete-items");
+            incompleteItems.appendChild(itemDiv);
+        }
+        else {
+            itemDiv.classList.add("completed");
+            var completedItems = document.getElementById("complete-items");
+            completedItems.appendChild(itemDiv);
+        }
+        var allTodos = getToDoItem();
     }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -22,7 +22,7 @@ function isValid() {
 }
 function getToDoItem(): ToDoItem {
   let myItem = new ToDoItem();
-  // get title
+
   let titleInput = getInput("title");
   myItem.title = titleInput.value;
 
@@ -39,28 +39,56 @@ function getInput(id): HTMLInputElement {
 function displayToDoItem(item: ToDoItem): void {
   let itemText = document.createElement("p");
   itemText.innerHTML = item.title;
+
   let itemDiv = document.createElement("div");
-  itemDiv.classList.add("todo");
-  if (item.isCompleted) {
-    itemDiv.classList.add("completed");
-  }
+  itemDiv.setAttribute("data-task-title", item.title);
+  itemDiv.onclick = toggleComplete;
 
   itemDiv.classList.add("todo");
   if (item.isCompleted) {
     itemDiv.classList.add("completed");
   }
 
-  function markAsComplete() {
-    let itemDiv = <HTMLElement>this;
+  itemDiv.classList.add("todo");
+  if (item.isCompleted) {
     itemDiv.classList.add("completed");
   }
+  itemDiv.appendChild(itemText);
+
   if (item.isCompleted) {
     let completedToDos = document.getElementById("complete-items");
     completedToDos.appendChild(itemDiv);
   } else {
     let incompleteToDos = document.getElementById("incomplete-items");
     incompleteToDos.appendChild(itemDiv);
-    
   }
-}
 
+
+  function markAsComplete() {
+    let itemDiv = <HTMLElement>this;
+    itemDiv.classList.add("completed");
+    console.log("Item div is:");
+    console.log(itemDiv);
+  }
+  function toggleComplete(){
+    let itemDiv = <HTMLElement>this;
+    console.log("Item div is:");
+    console.log(itemDiv);
+
+    if(itemDiv.classList.contains("completed")){
+        // Remove complete class if previously marked as completed
+        itemDiv.classList.remove("completed");
+        let incompleteItems = document.getElementById("incomplete-items");
+        incompleteItems.appendChild(itemDiv);
+    }
+    else{
+        // Add completed item to complete-items div
+        itemDiv.classList.add("completed");
+        let completedItems = document.getElementById("complete-items");
+        completedItems.appendChild(itemDiv);
+    }
+
+    let allTodos = getToDoItem();
+   
+    }
+}
